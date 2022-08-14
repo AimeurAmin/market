89557d97b479f920b8864b5fd4e049fdde81eacf
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-08-14 09:24:07 +0100

    login page design

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React, { DetailedHTMLProps, HTMLAttributes, MouseEventHandler } from "react";
+import style from "./button.module.scss";
+
+const { button } = style;
+
+interface ButtonProps {
+  text: string;
+  style: React.CSSProperties;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}
+
+const Button = ({ text, onClick, style }: ButtonProps) => {
+  return (
+    <div className={button} onClick={onClick} style={style}>
+      {text}
+    </div>
+  );
+};
+
+export default Button;
--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import Image from "../components/Image";
+import Login from "../features/authentication/Login";
+import styles from "./login-page.module.scss";
+import MarketManagerLogo from "../assets/imgs/Logo-market-manager.png";
+import { useNavigate } from "react-router-dom";
+const { container } = styles;
 const LoginPage = () => {
-  return <div>login-page</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={container}>
+      <Image
+        src={MarketManagerLogo}
+        alt="Market-Manager"
+        style={extrStyles.logo}
+        onClick={() => navigate("/")}
+      />
+      <Login />
+      {/* <Footer /> */}
+    </div>
+  );
 };
 
 export default LoginPage;
+
+const extrStyles = {
+  logo: {
+    width: 200,
+    position: "absolute",
+    top: 20,
+    left: 20,
+    cursor: "pointer",
+  },
+};
