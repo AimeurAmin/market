d1f73ded4d720a22d5b131698dc9e3d887872838
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-08-15 16:42:32 +0100

    top nav bar

--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,14 +1,16 @@
 import React, { PropsWithChildren } from "react";
 import styles from "./main-layout.module.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
 
-const { container, mainContent } = styles;
+const { container, mainContent, content } = styles;
 
 const MainLayout = (Content: any) => {
   return () => (
     <div className={container}>
       <Sidebar />
       <div className={mainContent}>
+        <Navbar />
         <Content />
       </div>
     </div>
