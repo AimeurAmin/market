dc3c10c2bd22c7e531e9195edb6a634797c3dcb6
Aimeur Amin <mohammedamin.aimeur@gmail.com>
2022-10-08 19:01:42 +0100

    adds global modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,25 @@
+import { createPortal } from 'react-dom';
+import styles from './modal.module.scss';
+
+const { container, overlay, content } = styles;
+
+interface ModalProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  close?: Function;
+}
+const Modal = ({children, isOpen, close}: ModalProps) => {
+  const portalDiv = document.getElementById('modal')!;
+  if(!isOpen) {
+    return null;
+  }
+  return createPortal(
+    <div className={container}>
+      <div className={overlay} onMouseUp={() => close? close() : undefined}></div>
+      <div className={content}>{children}</div>
+    </div>,
+    portalDiv
+  )
+}
+
+export default Modal;
