cfa04f0ae719411a2248a2ba7d736a65cdf2af4f
Aimeur Amin <mohammedamin.aimeur@gmail.com>
2022-10-08 19:02:20 +0100

    modal use example

--- src/features/cashier/components/cashierHeader/CashierHeader.tsx
+++ src/features/cashier/components/cashierHeader/CashierHeader.tsx
@@ -1,19 +1,22 @@
 import Button from '@components/Button/Button';
 import CustomInput from '@components/CustomInput/CustomInput';
-import Input from '@components/Input/Input';
-import React from 'react'
+import { useState } from 'react'
 import styles from './cashier-header.module.scss';
 import { MdSearch } from "react-icons/md";
 import { AiOutlineBarcode } from "react-icons/ai"
+import Modal from '@components/Modal/Modal';
 const { container, buttonStyle, inputContainer } = styles;
 
 const CashierHeader = () => {
-
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   const addProductHandler = () => {
-    alert('add Product')
+    setModalIsOpen(prev => !prev);
   }
   return (
     <div className={container}>
+      <Modal isOpen={modalIsOpen} close={() => setModalIsOpen(false)}>
+        <p>Hello</p>
+      </Modal>
       <Button text='Ajouter un produit' onClick={addProductHandler} className={buttonStyle}/>
       
       <CustomInput className={inputContainer}>
